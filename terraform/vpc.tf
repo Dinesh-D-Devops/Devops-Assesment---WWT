e113b09d4807d4774d1e7ef5c67fb2ffd4c5531f
Dinesh D <dinihanuma461@gmail.com>
2026-04-30 12:31:38 +0530

    Create vpc.tf

---
+++ terraform/vpc.tf
@@ -0,0 +1,32 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "mindmeld-vpc"
+  cidr = var.vpc_cidr
+
+  # Grab the first two Availability Zones in the region
+  azs             = slice(data.aws_availability_zones.available.names, 0, 2)
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true # Using a single NAT to save costs for the assessment
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  # CRITICAL: These tags allow EKS to automatically discover subnets to place Load Balancers
+  public_subnet_tags = {
+    "kubernetes.io/role/elb"                    = 1
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb"           = 1
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+}
